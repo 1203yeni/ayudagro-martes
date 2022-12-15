2dc53c6e6f43f5c21bf7417670a84b1d1fb23aae
DanierTrochez <danier.trochez@misena.edu.co>
2022-12-14 22:30:50 -0500

    validacion de email

--- src/views/RegistroView.vue
+++ src/views/RegistroView.vue
@@ -9,49 +9,71 @@
                 container-fluid
                 contenedor-registro">
 
-                <section class="registro col-8 col-sm-4 col-lg-4 col-xxl-5">
-                    <b-card-group deck>
-                        <b-card header-bg-variant="success" text-variant="black" header="REGISTRO" class="text-center" >
-                            <b-card-text>
-                                <form class="formulario">
-                                    <div class="container">
-                                        <div class="form-group left">
-                                            <label for="" class="control-label col-sm-7">Nombre</label>
-                                            <div class="col-sm-15">
-                                                <input type="text" class="form-control" name="nombre" id="nombre" v-model="form.nombre">
+                    <section class="registro col-8 col-sm-4 col-lg-4 col-xxl-5">
+                        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+                            <b-card-group deck>
+                                <b-card header-bg-variant="success" text-variant="black" header="REGISTRO"
+                                    class="text-center">
+                                    <b-card-text>
+                                        <form class="formulario">
+                                            <div class="container">
+                                                <div class="form-group left">
+                                                    <label for="" class="control-label col-sm-7">Nombre</label>
+                                                    <div class="col-sm-15">
+
+                                                        <input type="text" class="form-control" name="nombre"
+                                                            id="nombre" v-model="form.nombre">
+                                                    </div>
+                                                    <label for="" class="control-label col-sm-7">Apellido</label>
+                                                    <div class="col-sm-15">
+                                                        <input type="text" class="form-control" name="Apellido"
+                                                            id="Apellido" v-model="form.apellido">
+                                                    </div>
+
+                                                    <label for="" class="control-label col-sm-7">Nick_name</label>
+                                                    <div class="col-sm-15">
+                                                        <input type="text" class="form-control" name="Nick-name"
+                                                            id="Nick-name" v-model="form.nickName">
+                                                    </div>
+                                                    <label for="" class="control-label col-sm-7">Password</label>
+                                                    <div class="col-sm-15">
+                                                        <input type="password" class="form-control" name="Password"
+                                                            id="Password" v-model="form.PassWorld">
+                                                    </div>
+
+                                                    <div>
+
+                                                        <b-form-group id="input-group-1" label="Email address:"
+                                                            label-for="input-1" description="">
+                                                            <b-form-input id="input-1" class="form-control" name="Email"
+                                                                v-model="form.email" type="email"
+                                                                placeholder="Enter email" required></b-form-input>
+                                                        </b-form-group>
+
+
+
+                                                    </div>
+                                                    <label for="" class="control-label col-sm-7">Direccion</label>
+                                                    <div class="col-sm-15">
+                                                        <input type="text" class="form-control" name="Direccion"
+                                                            id="Direccion" v-model="form.direccion">
+                                                    </div>
+                                                    <br>
+
+                                                </div>
                                             </div>
-                                            <label for="" class="control-label col-sm-7">Apellido</label>
-                                            <div class="col-sm-15">
-                                                <input type="text" class="form-control" name="Apellido" id="Apellido" v-model="form.apellido">
+                                            <div class="form-group">
+                                                <b-button type="submit" class="boton" @click="GuardarUsuario()">Crear
+                                                    Usuario</b-button>
                                             </div>
-                                            <label for="" class="control-label col-sm-7">Nick_name</label>
-                                            <div class="col-sm-15">
-                                                <input type="text" class="form-control" name="Nick-name" id="Nick-name" v-model="form.nickName">
-                                            </div>
-                                            <label for="" class="control-label col-sm-7">Password</label>
-                                            <div class="col-sm-15">
-                                                <input type="text" class="form-control" name="Password" id="Password" v-model="form.PassWorld">
-                                            </div>
-                                            <label for="" class="control-label col-sm-7">Email</label>
-                                            <div class="col-sm-15">
-                                                <input type="text" class="form-control" name="Email" id="Email" v-model="form.Email">
-                                            </div>
-                                            <label for="" class="control-label col-sm-7">Direccion</label>
-                                            <div class="col-sm-15">
-                                                <input type="text" class="form-control" name="Direccion" id="Direccion" v-model="form.direccion">
-                                            </div>
-                                            <br>
-                                            
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <b-button class="boton" @click="GuardarUsuario()">Crear Usuario</b-button>
-                                    </div>
-                                </form>
-                            </b-card-text>
-                        </b-card>
-                    </b-card-group>
-                </section>
+
+                                        </form>
+
+                                    </b-card-text>
+                                </b-card>
+                            </b-card-group>
+                        </b-form>
+                    </section>
 
                 </section>
             </section>
@@ -60,53 +82,63 @@
 </template>
 
 <script>
-import axios from'axios'
+import axios from 'axios'
 export default {
-    name:"RegistroView",
-    data(){
-        return{
-            form:{
-            "idRol":1,  
-             "nombre":'',
-             "apellido":"",
-             "nickName":"",
-             "PassWorld":"",
-             "Email":"",
-             "direccion":""
-             
+    name: "RegistroView",
+    data() {
+        return {
+            form: {
+                email: '',
+                "idRol": 1,
+                "nombre": '',
+                "apellido": "",
+                "nickName": "",
+                "PassWorld": "",
+                "Email": "",
+                "direccion": ""
+
             },
-            listar:null,
+            foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+            show: true,
+            listar: null,
         }
     },
-    components:{
+    components: {
     },
-    methods:{
-      
-        GuardarUsuario() {  alert("registro exitoso")
-            axios.post("http://localhost:3000/nuevoAgricultor",this.form).then(response=>{
+    methods: {
+        onReset(event) {
+            event.preventDefault()
+            this.form.email = ''
+
+        },
+
+        GuardarUsuario() {
+            alert("registro exitoso")
+            axios.post("http://localhost:3000/nuevoAgricultor", this.form).then(response => {
                 console.log(response)
             })
             this.$router.push("/");
         }
     },
-    mounted(){
-    alert()
-    axios.get("http://localhost:3000/pregunta").then(response=>{
-      this.listar=response.data
-    })  
-  },
+    mounted() {
+        alert()
+        axios.get("http://localhost:3000/pregunta").then(response => {
+            this.listar = response.data
+        })
+    },
 }
 </script>
 
 <style scoped>
-.contenedor{
+.contenedor {
     margin: 10px;
 }
 
-.boton{
+.boton {
     background-color: rgb(27, 87, 27);
 }
-.formulario{
-    margin:10px;
+
+.formulario {
+    margin: 10px;
 }
 </style>
